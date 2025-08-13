cf906d1fb11311ab8f85901849dd3945ca8eab29
Ada <work@bprn.ru>
2025-08-13 17:07:37 +0700

    Продолжаю переделывать модуль template. Делаю генератор пунктов меню для создания разных Продуктов и Операций. Соответствующие редакторы помечаются аннотацией MenuItem

---
+++ src/main/java/ru/bprn/printhouse/StartupRunner.java
@@ -0,0 +1,20 @@
+package ru.bprn.printhouse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import ru.bprn.printhouse.views.templates.service.TemplatesMenuItemService;
+
+@Component
+public class StartupRunner implements ApplicationRunner {
+
+    @Autowired
+    private TemplatesMenuItemService scanner;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        scanner.scanAndUpdateMenuItems("ru/bprn/printhouse/views");
+    }
+}
+
